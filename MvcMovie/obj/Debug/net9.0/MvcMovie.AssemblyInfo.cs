be7faf13342f49ba1599bba06ef3253f8ec72450
Zahraa Goga <st10445649@vcconnect.edu.za>
2025-02-25 10:11:31 +0200

    third commit

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb3ac11056a6430f4f7c6738b4c0920c3cfb96d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f182946380b4e7c8aadbb93eb1af1cb1d187db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
