7f182946380b4e7c8aadbb93eb1af1cb1d187db6
Zahraa Goga <st10445649@vcconnect.edu.za>
2025-02-25 09:46:35 +0200

    Third commit

--- MvcMovie/Controllers/HelloWorld.cs
+++ MvcMovie/Controllers/HelloWorld.cs
@@ -13,8 +13,9 @@ public class HelloWorldController : Controller
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
-    {
-        return "This is the Welcome action method...";
-    }
+// Requires using System.Text.Encodings.Web;
+public string Welcome(string name, int ID = 1)
+{
+    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+}
 }
--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6134618e652db3b6d36a7c6835cf8999c66cd1a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb3ac11056a6430f4f7c6738b4c0920c3cfb96d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
